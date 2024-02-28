25eacd03d48af703ec2a85018c5766377e483047
ArielAVega <ariel.vega@educacionit.com>
2024-02-28 12:45:49 -0300

    Implement Security authentication with Spring Security and register/authenticate service

---
+++ src/main/java/com/vegasystems/repository/IUsuarioRepository.java
@@ -0,0 +1,13 @@
+package com.vegasystems.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.vegasystems.entity.Usuario;
+
+
+public interface IUsuarioRepository extends JpaRepository<Usuario, Long>{
+	Optional<Usuario> findByEmail(String email);
+
+}
